9fca3f5703231802d48a54ca74786985642742b7
SirPatschiii <tofue2003@gmx.de>
2023-12-05 21:48:54 +0100

    Commit of 05.12.2023

---
+++ src/05.12.23.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+print(add(10,10))
+print(subtract(10,10))
+print(multiply(10,10))
+print(divide(10,10))
