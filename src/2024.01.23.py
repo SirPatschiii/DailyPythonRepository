dd3075d1a8fc0bcde52ebe6db2b28f69742e1de5
SirPatschiii <tofue2003@gmx.de>
2024-01-23 18:31:29 +0100

    Commit of 23.01.2024

---
+++ src/2024.01.23.py
@@ -0,0 +1,42 @@
+import random
+import sys
+
+
+def random_array(length):
+    try:
+        length = int(length)
+    except:
+        print("Value needs to be a integer")
+        sys.exit(1)
+
+    if length < 1:
+        print("Value needs to be larger than 0")
+        sys.exit(1)
+    array = [0] * length
+    for i in range(length):
+        array[i] = i + 1
+    for i in range(100):
+        random.shuffle(array)
+    return array
+
+
+def search_element(array, key):
+    for i in range(len(array)):
+        if array[i] == key:
+            return i
+    return -1
+
+
+def main():
+    print("This program performs sequential searches.")
+    array = random_array(input("How big should your array be? "))
+    v_key = int(input("What number do you search? "))
+    v_index = search_element(array, v_key)
+
+    if v_index == -1:
+        print("The searched value isn't in the array.")
+    print(f"Your number {v_key} is located at the index {v_index}.")
+
+
+if __name__ == '__main__':
+    main()
