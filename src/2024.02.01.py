c522d0e3a5ba49140a0a14329f1f321fdceb9681
SirPatschiii <tofue2003@gmx.de>
2024-02-01 12:38:59 +0100

    Corrected a mistake

--- src/2024.02.01.py
+++ src/2024.02.01.py
@@ -3,7 +3,9 @@ def caesar_encryption_algorithm(p_string):
     v_encrypted = ""
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c']
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
+                'b', 'c', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', '1', '2', '3']
 
     for character in p_string:
         index = 0
