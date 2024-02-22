28e25f660c04cdad14fbfaafbb25eefff991e6a3
SirPatschiii <tofue2003@gmx.de>
2024-02-22 09:03:43 +0100

    Commit of 22.02.2024

---
+++ src/2024.02.22.py
@@ -0,0 +1,33 @@
+from Crypto.Cipher import ChaCha20
+from Crypto.Random import get_random_bytes
+
+
+def cha_cha_20_encrypt(p_key, p_nonce, p_plaintext):
+    cipher = ChaCha20.new(key=p_key, nonce=p_nonce)
+    ciphertext = cipher.encrypt(p_plaintext)
+    return ciphertext
+
+
+def cha_cha_20_decrypt(p_key, p_nonce, p_ciphertext):
+    cipher = ChaCha20.new(key=p_key, nonce=p_nonce)
+    plaintext = cipher.decrypt(p_ciphertext)
+    return plaintext
+
+
+def main():
+    print("This script encrypts and decrypts a String with the ChaCha20 Cipher.")
+
+    key = get_random_bytes(32)
+    nonce = get_random_bytes(8)
+
+    plaintext = input("Please insert your String to be encrypted: ").encode("utf-8")
+
+    ciphertext = cha_cha_20_encrypt(key, nonce, plaintext)
+    print(f"Cipher String: {ciphertext.hex()}")
+
+    decrypted_text = cha_cha_20_decrypt(key, nonce, ciphertext)
+    print(f"Decrypted String: {decrypted_text.decode()}")
+
+
+if __name__ == '__main__':
+    main()
